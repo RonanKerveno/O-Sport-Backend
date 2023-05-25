25a40661150de00560ed075611801c6bb0c065bb
RonanKerveno <ronan.kerveno@gmail.com>
2023-05-25 11:02:39 +0200

    Favorites sports added to createOneUser and updateOneUser controllers

--- app/middlewares/bodySanitizer.js
+++ app/middlewares/bodySanitizer.js
@@ -1,14 +1,10 @@
 const sanitizer = require('sanitizer');
 
 const bodySanitizer = (req, res, next) => {
-  // Si on a un body, autrement dit, si on est sur autre chose qu'une requete get
   if (req.body) {
-    // Pour chaque propriété dans le body (par exemple .name, .position, etc...)
     Object.keys(req.body).forEach((propName) => {
-      if (Object.prototype.hasOwnProperty.call(req.body, propName)) {
+      if (typeof req.body[propName] === 'string') {
         req.body[propName] = sanitizer.escape(req.body[propName]);
-        // Par exemple pour le name, le traitement ressemble à ça :
-        // req.body.name = sanitizer.escape(req.body.name);
       }
     });
   }
